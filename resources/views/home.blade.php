caa20ad54c38e8101fadd430a6d6f29ecfc22738
LaFunAttitude-Dev <feltsean43770@gmail.com>
2022-02-24 15:35:37 +0100

    add games

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,31 @@
 @section('title', 'Accueil')
 
 @section('content')
-    <div class="container">
-
+    <div class="container text-center pt-4">
+        <h1>
+            Join Us
+        </h1>
+        <i class="fa-brands fa-discord"></i>
+        <i class="fa-brands fa-twitter"></i>
     </div>
+
+    <section id="games">
+        <div class="d-flex">
+            <div class="rainbow-six-siège">
+                <img src="{{ asset('assets/img/characters/Rainbow%20six%20siège.png') }}" alt="Rainbow Six Siège">
+            </div>
+            <div class="fortnite">
+                <img src="{{ asset('assets/img/characters/Fortnite.png') }}" alt="Fortnite">
+            </div>
+            <div class="league-of-legends">
+                <img src="{{ asset('assets/img/characters/League%20of%20legends.png') }}" alt="League Of Legends">
+            </div>
+            <div class="valorant">
+                <img src="{{ asset('assets/img/characters/Valorant.png') }}" alt="Valorant">
+            </div>
+            <div class="battlefield">
+                <img src="{{ asset('assets/img/characters/Battlefield.png') }}" alt="Battlefield">
+            </div>
+        </div>
+    </section>
 @endsection

00f9d92547b68056f04e29c49d8e5430c0180489
LaFunAttitude-Dev <feltsean43770@gmail.com>
2022-02-25 14:30:07 +0100

    modify page

--- resources/views/p/r6.blade.php
+++ resources/views/p/r6.blade.php
@@ -12,14 +12,77 @@
     </div>
 
     <section id="r6">
-        <div class="container pt-5">
-            <div class="life-time">
-                <div class="card bg-transparent">
-                    <img src="{{ asset('assets/img/Logos/triangle-r6.png') }}" alt=""><h5 class="card-header"> Featured</h5>
-                    <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <div class="container py-5">
+            <div class="row">
+                <div class="col-md-9">
+                    <div class="r6-card mb-3">
+                        <div class="card bg-transparent">
+                            <h5 class="card-header">
+                                <img src="{{ asset('assets/img/Logos/triangle-r6.png') }}" alt="">
+                                LifeTime
+                            </h5>
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    Special title treatment
+                                </h5>
+                                <p class="card-text">
+                                    With supporting text below as a natural lead-in to additional
+                                    content.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
 
+                    <div class="r6-card w-75">
+                        <div class="card bg-transparent">
+                            <h5 class="card-header">
+                                <img src="{{ asset('assets/img/Logos/triangle-r6.png') }}" alt="">
+                                Ranked
+                            </h5>
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    Special title treatment
+                                </h5>
+                                <p class="card-text">
+                                    With supporting text below as a natural lead-in to additional
+                                    content.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-md-3">
+                    <div class="r6-card p-2 mb-5">
+                        <div class="container p-2">
+                            <div>
+                                <p>173 -> 174</p>
+                                <div class="progress">
+                                    <div class="progress-bar bg-dark" role="progressbar" style="width: 25%;"
+                                         aria-valuenow="25"
+                                         aria-valuemin="0" aria-valuemax="100">25%
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="r6-card pb-2">
+                        <div class="card bg-transparent">
+                            <h5 class="card-header">
+                                <img src="{{ asset('assets/img/Logos/medaille.png') }}" alt="">
+                                Meilleurs Opérateurs
+                            </h5>
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    Special title treatment
+                                </h5>
+                                <p class="card-text">
+                                    With supporting text below as a natural lead-in to additional content.
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>

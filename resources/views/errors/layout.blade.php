f40bcc5fecdd3bbf77271e35d99045c2a3d0ec69
LaFunAttitude-Dev <feltsean43770@gmail.com>
2022-03-04 10:31:13 +0100

    modify style

--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -1,18 +1,24 @@
-@extends('layouts.app')
+@extends('layouts.errors')
 
 @section('content')
     <div class="container content error-page">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ trans('errors.error') }}</div>
+        <div class="row center">
+            <div class="col col-md-8">
+                <div class="card bg-transparent">
+                    <div class="card-header bg-black mb-2">{{ trans('errors.error') }}</div>
 
-                    <div class="card-body text-center">
-                        <h1>@yield('code')</h1>
-                        <h2>@yield('title')</h2>
-                        <p>@yield('message')</p>
+                    <div class="card-body bg-dark text-center">
+                        <h1 class="pb-4 text-white">
+                            @yield('code')
+                        </h1>
+                        <h2 class="pb-2">
+                            @yield('title')
+                        </h2>
+                        <p>
+                            @yield('message')
+                        </p>
 
-                        <a href="{{ route('home') }}" class="btn btn-primary btn-grad">{{ trans('errors.home') }}</a>
+                        <a href="{{ route('home') }}" class="btn btn-principal">{{ trans('errors.home') }}</a>
                     </div>
                 </div>
             </div>

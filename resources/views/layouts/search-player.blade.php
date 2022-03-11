5130791396b67022dc51a8bc511b25e5c7fec58b
LaFunAttitude-Dev <feltsean43770@gmail.com>
2022-03-11 13:39:11 +0100

    fix

--- resources/views/layouts/search-player.blade.php
+++ resources/views/layouts/search-player.blade.php
@@ -74,11 +74,9 @@
         <link href="{{ asset('assets/css/variables.css') }}" rel="stylesheet">
         <link href="{{ asset('assets/css/responsive.css') }}" rel="stylesheet">
         <link href="{{ asset('assets/css/player.css') }}" rel="stylesheet">
-        <link href="{{ asset('assets/css/navbar.css') }}" rel="stylesheet">
-        <link href="{{ asset('assets/css/main.css') }}" rel="stylesheet">
+        <link href="{{ asset('assets/css/index.css') }}" rel="stylesheet">
         <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet">
         <link href="{{ asset('assets/css/widgets.css') }}" rel="stylesheet">
-        <link href="{{ asset('assets/css/footer.css') }}" rel="stylesheet">
         @stack('styles')
     </head>
 

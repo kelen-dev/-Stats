66d4c80223480376fb3112669b09d2a3da7a3e35
LaFunAttitude-Dev <feltsean43770@gmail.com>
2022-03-15 14:25:06 +0100

    rename title for #games

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
     <section class="py-5" id="games">
         <div class="ps-4 title">
             <h2 class="h3">
-                Games
+                Recherchez un joueur
             </h2>
             <p class="separator"></p>
         </div>
@@ -22,26 +22,46 @@
             <div class="rainbow-six-siège">
                 <a href="{{ route('search-r6') }}">
                     <img src="{{ asset('assets/img/characters/Rainbow%20six%20siège.png') }}" alt="Rainbow Six Siège">
+
+                    <div class="content d-flex justify-content-center text-white">
+                        <p>Rainbow six Siège</p>
+                    </div>
                 </a>
             </div>
             <div class="fortnite">
                 <a href="{{ route('search-fortnite') }}">
                     <img src="{{ asset('assets/img/characters/Fortnite.png') }}" alt="Fortnite">
+
+                    <div class="content d-flex justify-content-center text-white">
+                        <p>Fortnite</p>
+                    </div>
                 </a>
             </div>
             <div class="league-of-legends">
                 <a href="{{ route('search-lol') }}">
                     <img src="{{ asset('assets/img/characters/League%20of%20legends.png') }}" alt="League Of Legends">
+
+                    <div class="content d-flex justify-content-center text-white">
+                        <p>League of Legends</p>
+                    </div>
                 </a>
             </div>
             <div class="valorant">
                 <a href="{{ route('search-valorant') }}">
                     <img src="{{ asset('assets/img/characters/Valorant.png') }}" alt="Valorant">
+
+                    <div class="content d-flex justify-content-center text-white">
+                        <p>Valorant</p>
+                    </div>
                 </a>
             </div>
             <div class="battlefield">
                 <a href="{{ route('search-battlefield') }}">
                     <img src="{{ asset('assets/img/characters/Battlefield.png') }}" alt="Battlefield">
+
+                    <div class="content d-flex justify-content-center text-white">
+                        <p>Battlefield</p>
+                    </div>
                 </a>
             </div>
         </div>

c691d76f39b9509f2ca30e79331fe89a1eb94ad4
LaFunAttitude-Dev <feltsean43770@gmail.com>
2022-03-15 14:23:15 +0100

    add search button

--- resources/views/p/search-valorant.blade.php
+++ resources/views/p/search-valorant.blade.php
@@ -15,8 +15,13 @@
                 <div class="col col-md-1 game game-valorant">
                     <img src="{{ asset('assets/img/Search/ps.png') }}" alt="playstation">
                 </div>
-                <div class="col col-md-9 search">
+                <div class="col col-md-8 search">
                     Recherchez un joueur
+                </div>
+                <div class="col col-md-1 bg-secondary d-flex justify-content-center align-items-center">
+                    <a href="{{ route('valorant') }}">
+                        <i class="text-dark fa-solid fa-magnifying-glass"></i>
+                    </a>
                 </div>
             </div>
         </div>

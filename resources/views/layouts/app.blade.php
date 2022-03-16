1822f8afe2c1e05506bb7711c8d3a3d9410805b0
LaFunAttitude-Dev <feltsean43770@gmail.com>
2022-03-16 10:02:51 +0100

    delete player music

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -86,27 +86,6 @@
             <i class="fas fa-angle-up"></i>
         </a>
 
-        {{-- Music player --}}
-        <div class="player">
-            <div class="player-track">
-                <div class="artist-name"></div>
-                <div class="music-name"></div>
-                <div class="progress-bar">
-                    <div class="fillBar"></div>
-                </div>
-                <div class="time"></div>
-            </div>
-            <div class="circle">
-                <div class="circ"></div>
-                <div id="thumbnail" class="thumbnail"></div>
-            </div>
-            <div class="player-control">
-                <i id="prev" class="prev-btn fas fa-backward"></i>
-                <i id="play" class="play-btn fas fa-play"></i>
-                <i id="next" class="next-btn fas fa-forward"></i>
-            </div>
-        </div>
-
         @include('flashy::message')
 
         {{-- Page --}}
